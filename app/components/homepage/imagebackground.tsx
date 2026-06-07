850fda9baa98d98a8454f3b942c2b572cdeb992f
Ian Ramos <ianflorentinramos@gmail.com>
2026-06-07 20:41:02 -0300

    creacion carpetas , homepage, landingpage y componentes de homepage

---
+++ app/components/homepage/imagebackground.tsx
@@ -0,0 +1,8 @@
+export function imageBackground(){
+    return(
+        <>
+            <img className="aspect-3/2" src="assets/background1"/>
+            
+        </>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-orange-50 text-orange-600">
-      <h1 className="text-5xl font-bold font-sans">
-        ¡Bienvenido a Fire! 🔥
-      </h1>
-      <p className="mt-4 text-xl">
-        Tu app de productividad gamificada.
-      </p>
-    </main>
-  );
+  redirect("/layout/homepage")
 }
